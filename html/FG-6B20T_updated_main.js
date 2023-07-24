452506606131ab3cd29d824f335b3348759598c3
Janet Chang <janet.chang@mssm.edu>
2023-07-24 16:50:56 -0400

    fix spaces in video file unknown resource error

--- html/FG-6B20T_updated_main.js
+++ html/FG-6B20T_updated_main.js
@@ -210,7 +210,7 @@ async function experimentInit() {
     win: psychoJS.window,
     name: 'ins_video',
     units: psychoJS.window.units,
-    movie: 'stimuli/Which Pond Tutorial Video_FINAL_3.1.23.mp4',
+    movie: 'stimuli/WhichPondTutorialVideo_FINAL_3.1.23.mp4',
     pos: [0, 0],
     anchor: 'center',
     size: [0.9, 0.5],
